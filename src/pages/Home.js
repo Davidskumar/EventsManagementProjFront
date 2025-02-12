a8e91ccbcb6fed4cea62197186b7991e400ac2a8
RusticByte <81314017+Davidskumar@users.noreply.github.com>
2025-02-12 18:14:40 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <h1>Welcome to Home Page</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <button onClick={() => navigate("/login")}>Go to Login</button>
+    </div>
+  );
 };
 
 export default Home;
